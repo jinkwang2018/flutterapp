991fee4bf393b21b55d6eb562d7119cb03e72e6b
jkkang <kjkkjk2002@gmail.com>
2020-05-19 09:21:19 +0900

    flutter app sample end
    flutter app sample end

--- lib/main.dart
+++ lib/main.dart
@@ -1,50 +1,157 @@
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(
-    MaterialApp(
-      title: 'Flutter Layout Demo', // 앱의 타이틀
-      theme: ThemeData( // 앱의 테마 설정
-        primarySwatch: Colors.red,  // 주요 테마 색상
+// Todo 리스트의 아이템을 저장할 자료 구조
+void main() {
+  runApp(MaterialApp(
+    title: 'Passing Data',
+    home: MyApp(
+      // TodosScreen 스크린을 홈으로 설정
+      // Todo 타입의 아이템을 20개 List로 생성하여 전달
+      todos: List.generate(
+        20, // 생성할 아이템 갯수
+        (i) => Todo(
+            'Todo $i', 'A description of what needs to be done for Todo $i'),
       ),
-      // initialRoute 설정. home이 아닌 초기 라우트로 설정
-      // '/'은 앱의 첫 라우트(root 라우트)를 의미함
-      initialRoute: '/',
-
-      // 라우트들을 등록
-      routes: {
-        '/': (context) => FirstScreen(),  // 초기 라우트로 FirstScreen을 설정
-        '/second': (context) => SecondScreen(), // '/second'란 경로로 SecondScrrent을 설정정
-      },  
-    )
-  );
+    ),
+  ));
 }
 
-// 첫번째 라우트
-class FirstScreen extends StatelessWidget{
+class Todo {
+  // final로 정의된 필드는 선언단계나 생성자를 통해 초기화해야함
+  final String title;
+  final String description;
+
+  Todo(this.title, this.description); // 생성자로 필드 초기화
+}
+class SnackBarDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // MaterialApp 생성/반환
+    return MaterialApp(
+      title: 'Snackbar Demo',
+      // 홈으로 Scaffold을 직접 구현
+      home: Scaffold(
+        appBar: AppBar(title: Text('SnackBar Demo')),
+        // SncacBarpage 클래스 호출
+        body: SnackBarPage(),
+      ),
+    );
+  }
+}
+
+// 초기 스크린의 body를 구성
+class SnackBarPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      // 화면 중앙에 RaisedButton 추가
+      child: RaisedButton(
+        // RaisedButton에 텍스트 추가
+        child: Text('Show SnackBar'),
+        // RaisedButton을 누르면 호출
+        onPressed: () {
+          // 스낵바 생성
+          final snackbar = SnackBar(
+            // 컨텐트 구성. 텍스를 주입
+            content: Text('Yay! A SncackBar!'),
+            // 액션 구입
+            action: SnackBarAction(
+              // 스낵바에 label 추가
+              label: 'Undo',
+              // 스낵바에 label을 누를 때 호출
+              onPressed: () {
+                // Some code to undo the change.
+              },
+            ),
+          );
+          
+          // 생성한 스낵바를 노출. 화면 하단에 스낵바가 출력됨
+          Scaffold.of(context).showSnackBar(snackbar);
+        },
+      ),
+    );
+  }
+}
+
+// 앱 실행시 출력되는 라우트
+class MyApp extends StatelessWidget{
+  final List<Todo> todos; // Todo 아이템 리스트
+ // 생성자에서 Todo 아이템 리스트를 받아서 필드에 저장
+  MyApp({Key key, @required this.todos}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       appBar: AppBar(
-          title: Text('Flutter Layout Demo')
+        title: Text('Home Screen Home Screen'),
+        
       ),
-      body: Center(
-        // RaisedButton을 자식으로 등록
-        // RaisedButton: 평평한 레이아웃 위에 올라와있는 형태의 버튼
-        // 반대로 평평한 모양을 가지는 FlatButton 도 존재함
-        child: RaisedButton(
-          child: Text('Open second route'),
-          onPressed: (){  // 버튼이 눌렸을 때 처리
-            // 라우트의 이름으로 새로운 라우트 적재
-            Navigator.pushNamed(context, '/second');
-          },
+      body: ListView.builder(
+        // 리스트뷰의 아이템 갯수. itemCount의 수만큼 itemBuilder가 호출됨
+        itemCount: todos.length,
+        // 리스트뷰의 아이템 생성
+        itemBuilder: (context, index) {
+          // ListTile을 반환
+          return ListTile(
+            title: Text(todos[index].title),
+            // ListTile은 onTap과 onLongPress를 지원함함
+            onTap: () {
+              // ListTile이 탭되면 DetailScreen 스크린을 적재
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  // 파라미터 todo로 tap된 index의 아이템을 전달
+                  builder: (context) => DetailScreen(todo: todos[index]),
+                ),
+              );
+            },
+          );
+        },
+      ),// 드로워(서랍) 추가
+      drawer: Drawer(
+        // 리스트뷰 추가
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            // 드로워해더 추가
+            DrawerHeader(
+              child: Text('Drawer Header'),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+            ),
+            // 리스트타일 추가
+            ListTile(
+              title: Text('Item 1'),
+              onTap: (){
+               SnackBarDemo();
+              },
+            ),
+            // 리스트타일 추가
+            ListTile(
+              title: Text('Item 2'),
+              onTap: (){
+                // 드로워를 닫음
+                Navigator.pop(context);
+              },
+            )
+          ],
         ),
       ),
     );
   }
 }
-class SecondScreen extends StatelessWidget {
+
+class DetailScreen  extends StatelessWidget{
+  final Todo todo;
+
+  // 생성자. 라우트 생성시 전달한 파라미터를 아규먼트로 넘겨받아 필드에 저장
+  const DetailScreen({
+    Key key,
+    @required this.todo
+  }):super(key:key);
+
   @override
   Widget build(BuildContext context) {
     // 본 앱의 테마의 대표색상을 필드에 저장
@@ -60,11 +167,66 @@ class SecondScreen extends StatelessWidget {
         ],
       ),
     );
+  
+    // 컨테이터 위젯 구현
+    Widget titleSection = Container(
+      // 컨테이너 내부 상하좌우에 32픽셀만큼의 패딩 삽입
+      padding: const EdgeInsets.all(32),
+      // 자식으로 로우를 추가
+      child: Row(
+        // 로우에 위젯들(Expanded, Icon, Text)을 자식들로 추가
+        children: <Widget>[
+          // 첫번째 자식
+          Expanded(
+            // 첫번째 자식의 자식으로 컬럼 추가
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start, // 자식들을 왼쪽정렬로 배치함
+              // 컬럼의 자식들로 컨테이너와 텍스트를 추가
+              children: <Widget>[
+                // 컬럼의 첫번째 자식 컨테이너
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8), // 컨테이너 내부 하단에 8픽셀만큼 패딩 삽입
+                  child: Text(todo.title,  // 컨테이너의 자식으로 텍스트 삽입
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold // 텍스트 강조 설정
+                    ),
+                  ),
+                ),
+                // 컬럼의 두번째 자식으로 텍스트 삽입
+                Text(
+                  todo.description,
+                  style: TextStyle(
+                      color: Colors.grey[500] // 텍스트의 색상을 설정
+                  ),
+                )
+              ],
+            ),
+          ),
+          // 두번째 자식 아이콘
+          // Icon(
+          //   Icons.star, // 별모양 아이콘 삽입
+          //   color: Colors.red[500], // 빨간색으로 설정
+          // ),
+          // // 세번째 자식
+          // Text('43')  // 텍스트 표시
+          FavoriteWidget()
+        ],
+      ),
+    );
+
+    // 텍스트로 구성된 컨테이너를 구현하는 위젯
+    Widget textSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Text(
+        todo.description,
+        softWrap: true, // 텍스트가 영역을 넘어갈 경우 줄바꿈 여부
+      ),
+    );
 
     // 매트리얼앱을 생성하여 반환
     return Scaffold(
       appBar: AppBar(
-        title: Text("Flutter layout demo"),
+        title: Text(todo.title),
       ),
       body: ListView( // 리스트로 교체
         // 자식들을 추가
@@ -76,60 +238,13 @@ class SecondScreen extends StatelessWidget {
             height: 240,
             fit: BoxFit.cover,
           ),
-          titleSection,  // titleSection 컨테이너 추가
+          titleSection,
           buttonSection,
           textSection,
         ],
       ),
     );
   }
-   // 컨테이터 위젯 구현
-  Widget titleSection = Container(
-    // 컨테이너 내부 상하좌우에 32픽셀만큼의 패딩 삽입
-    padding: const EdgeInsets.all(32),
-    // 자식으로 로우를 추가
-    child: Row(
-      // 로우에 위젯들(Expanded, Icon, Text)을 자식들로 추가
-      children: <Widget>[
-        // 첫번째 자식
-        Expanded(
-          // 첫번째 자식의 자식으로 컬럼 추가
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start, // 자식들을 왼쪽정렬로 배치함
-            // 컬럼의 자식들로 컨테이너와 텍스트를 추가
-            children: <Widget>[
-              // 컬럼의 첫번째 자식 컨테이너
-              Container(
-                padding: const EdgeInsets.only(bottom: 8), // 컨테이너 내부 하단에 8픽셀만큼 패딩 삽입
-                child: Text(  // 컨테이너의 자식으로 텍스트 삽입
-                  "Oeschinen Lake Campground",
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold // 텍스트 강조 설정
-                  ),
-                ),
-              ),
-              // 컬럼의 두번째 자식으로 텍스트 삽입
-              Text(
-                'Kandersteg, Switzerland',
-                style: TextStyle(
-                    color: Colors.grey[500] // 텍스트의 색상을 설정
-                ),
-              )
-            ],
-          ),
-        ),
-        // 두번째 자식 아이콘
-        // Icon(
-        //   Icons.star, // 별모양 아이콘 삽입
-        //   color: Colors.red[500], // 빨간색으로 설정
-        // ),
-        // // 세번째 자식
-        // Text('43')  // 텍스트 표시
-        FavoriteWidget()
-      ],
-    ),
-  );
-
   // 버튼과 텍스트로 구성된 컬럼을 생성하여 반환하는 함수
   Column _buildButtonColumn(Color color,IconData icon, String label){
     // 컬럼을 생성하여 반환
@@ -156,23 +271,17 @@ class SecondScreen extends StatelessWidget {
       ],
     );
   }
-
-  // 텍스트로 구성된 컨테이너를 구현하는 위젯
-  Widget textSection = Container(
-    padding: const EdgeInsets.all(32),
-    child: Text(
-      'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
-          'Alps. Situated 1,578 meters above sea level, it is one of the '
-          'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
-          'half-hour walk through pastures and pine forest, leads you to the '
-          'lake, which warms to 20 degrees Celsius in the summer. Activities '
-          'enjoyed here include rowing, and riding the summer toboggan run.',
-      softWrap: true, // 텍스트가 영역을 넘어갈 경우 줄바꿈 여부
-    ),
-  );
 }
 
-// StatefulWidget을 상속하는 클래스
+// 라우트 생성시 전달할 아규먼트 클래스
+class ScreenArguments{
+  // 아규먼트의 타이틀과 메시지. 생성자에 의해서만 초기화되고 변경할 수 없음
+  final String title;
+  final String message;
+
+  // 생성자
+  ScreenArguments(this.title, this.message);
+}
 class FavoriteWidget extends StatefulWidget {
   @override
   _FavoriteWidgetState createState() => _FavoriteWidgetState();
--- lib/sample.dart
+++ lib/sample.dart
@@ -1,195 +1,224 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
 
-class RandomWords extends StatefulWidget{
-  @override
-  RandomWordsState createState() => new RandomWordsState();
+void main(){
+  runApp(
+    MaterialApp(
+      title: 'Flutter Layout Demo', // 앱의 타이틀
+      theme: ThemeData( // 앱의 테마 설정
+        primarySwatch: Colors.red,  // 주요 테마 색상
+      ),
+      // initialRoute 설정. home이 아닌 초기 라우트로 설정
+      // '/'은 앱의 첫 라우트(root 라우트)를 의미함
+      initialRoute: '/',
+
+      // 라우트들을 등록
+      routes: {
+        '/': (context) => FirstScreen(),  // 초기 라우트로 FirstScreen을 설정
+        '/second': (context) => SecondScreen(), // '/second'란 경로로 SecondScrrent을 설정정
+      },  
+    )
+  );
 }
-class RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
-  final _biggerFont = const TextStyle(fontSize: 18.0);
+
+// 첫번째 라우트
+class FirstScreen extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-        title: Text('Startup Name Generator'),
-    ),
-    body: _buildSuggestions(),
-    );
-  }
-
-  Widget _buildSuggestions() {
-    return ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        itemBuilder: /*1*/ (context, i) {
-          if (i.isOdd) return Divider(); /*2*/
-
-          final index = i ~/ 2; /*3*/
-          if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10)); /*4*/
-          }
-          return _buildRow(_suggestions[index]);
-        });
-  }
-
-  Widget _buildRow(WordPair pair) {
-    return ListTile(
-      title: Text(
-        pair.asPascalCase,
-        style: _biggerFont,
+      appBar: AppBar(
+          title: Text('Flutter Layout Demo')
+      ),
+      body: Center(
+        // RaisedButton을 자식으로 등록
+        // RaisedButton: 평평한 레이아웃 위에 올라와있는 형태의 버튼
+        // 반대로 평평한 모양을 가지는 FlatButton 도 존재함
+        child: RaisedButton(
+          child: Text('Open second route'),
+          onPressed: (){  // 버튼이 눌렸을 때 처리
+            // 라우트의 이름으로 새로운 라우트 적재
+            Navigator.pushNamed(context, '/second');
+          },
+        ),
       ),
     );
   }
 }
-
-class MyApp extends StatelessWidget {
+class SecondScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-       theme: ThemeData(
-         primarySwatch: Colors.blue, 
-         accentColor: Colors.pinkAccent, 
-         fontFamily: 'Ubuntu',
-       ),
+    // 본 앱의 테마의 대표색상을 필드에 저장
+    Color color = Theme.of(context).primaryColor;
+    // 버튼 로우 구성을 위한 컨테이너 위젯
+    Widget buttonSection = Container(
+      child: Row( // 로우를 자식으로 가짐
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly, // 자식들이 여유 공간을 공편하게 나눠가짐
+        children: <Widget>[ // 세개의 위젯들을 자식들로 가짐
+          _buildButtonColumn(color, Icons.call, 'CALL'),
+          _buildButtonColumn(color, Icons.near_me, 'ROUTH'),
+          _buildButtonColumn(color, Icons.share, 'SHARE')
+        ],
+      ),
+    );
 
-      title: 'Startup Name Generator',
-      home: new Scaffold(
-        resizeToAvoidBottomPadding: false,
-        appBar: new AppBar( 
-          title: new Text("ee"), 
-          actions: <Widget>[ 
-            new IconButton( 
-              icon: new Icon(Icons.photo_album), 
-              tooltip: 'Hi!', 
-              onPressed: () => {}, 
-            ), 
-            new IconButton( 
-              icon: new Icon(Icons.pie_chart), 
-              tooltip: 'Wow', 
-              onPressed: () => {}, 
-            ) 
-          ], 
-        ),
-        drawer: Drawer(
-          // Add a ListView to the drawer. This ensures the user can scroll
-          // through the options in the drawer if there isn't enough vertical
-          // space to fit everything.
-          child: ListView(
-            // Important: Remove any padding from the ListView.
-            padding: EdgeInsets.zero,
-            children: <Widget>[
-              DrawerHeader(
-                child: Text('Drawer Header'),
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                ),
-              ),
-              ListTile(
-                title: Text('Item 1'),
-                onTap: () {
-                  // Update the state of the app
-                  // ...
-                  // Then close the drawer
-                  Navigator.pop(context);
-                },
-              ),
-              ListTile(
-                title: Text('Item 2'),
-                onTap: () {
-                  // Update the state of the app
-                  // ...
-                  // Then close the drawer
-                  Navigator.pop(context);
-                },
-              ),
-            ],
+    // 매트리얼앱을 생성하여 반환
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Flutter layout demo"),
+      ),
+      body: ListView( // 리스트로 교체
+        // 자식들을 추가
+        children: <Widget>[
+          // 이미지 추가
+          Image.asset(
+            'images/cat.jpg',
+            width: 600,
+            height: 240,
+            fit: BoxFit.cover,
           ),
-        ),
-        bottomNavigationBar: new BottomNavigationBar( 
-          items: [ 
-            new BottomNavigationBarItem( 
-              icon: new Icon(Icons.account_balance), 
-              title: new Text('Bank'), 
-            ), 
-            new BottomNavigationBarItem( 
-              icon: new Icon(Icons.apps), 
-              title: new Text('Apps'), 
-            ), 
-            new BottomNavigationBarItem( 
-              icon: new Icon(Icons.shopping_basket), 
-              title: new Text('Shop'), 
-            ), 
-          ],
-        ),
-        body: new Container( 
-          padding: const EdgeInsets.all(20.0), 
-          child: new Column( 
-            children: <Widget>[
-              new Text("Hello World"),
-              new TextField( 
-                decoration: new InputDecoration( 
-                  border: InputBorder.none, 
-                  hintText: 'Please enter text!!!'
-                  ), 
-              ),
-              new Checkbox( 
-                value: true, 
-                onChanged: (bool value) {}, 
-              ), 
-              new Radio<int>( 
-                value: 0, 
-                groupValue: 0, 
-                onChanged: (_) {}, 
-              ),
-              new Switch( 
-                value: false, 
-                onChanged: (bool value) {}, 
-              ),
-              new ButtonBar( 
-                mainAxisSize: MainAxisSize.min, 
-                children: <Widget>[ 
-                  new RaisedButton( 
-                    child: const Text('Raised'), 
-                    onPressed: null, 
-                  ), 
-                  const RaisedButton( 
-                    child: const Text('Disabled'), 
-                    onPressed: null, 
-                  ), 
-                ], 
-              ), 
-              new ButtonBar( 
-                mainAxisSize: MainAxisSize.min, 
-                children: <Widget>[ 
-                  new FlatButton( 
-                    child: const Text('Flat'), 
-                    onPressed: null, 
-                  ), 
-                  const FlatButton( 
-                    child: const Text('Disabled'), 
-                    onPressed: null, 
-                  ), 
-                ], 
-              ), 
-              new ButtonBar( 
-                mainAxisSize: MainAxisSize.min, 
-                children: <Widget>[ 
-                  new OutlineButton( 
-                    child: const Text('OutLine'), 
-                    onPressed: null, 
-                  ), 
-                  const RaisedButton( 
-                    child: const Text('Disabled'), 
-                    onPressed: null, 
-                  ), 
-                ], 
-              ),
-            ],
-          ),
-        ),
+          titleSection,  // titleSection 컨테이너 추가
+          buttonSection,
+          textSection,
+        ],
       ),
     );
   }
+   // 컨테이터 위젯 구현
+  Widget titleSection = Container(
+    // 컨테이너 내부 상하좌우에 32픽셀만큼의 패딩 삽입
+    padding: const EdgeInsets.all(32),
+    // 자식으로 로우를 추가
+    child: Row(
+      // 로우에 위젯들(Expanded, Icon, Text)을 자식들로 추가
+      children: <Widget>[
+        // 첫번째 자식
+        Expanded(
+          // 첫번째 자식의 자식으로 컬럼 추가
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start, // 자식들을 왼쪽정렬로 배치함
+            // 컬럼의 자식들로 컨테이너와 텍스트를 추가
+            children: <Widget>[
+              // 컬럼의 첫번째 자식 컨테이너
+              Container(
+                padding: const EdgeInsets.only(bottom: 8), // 컨테이너 내부 하단에 8픽셀만큼 패딩 삽입
+                child: Text(  // 컨테이너의 자식으로 텍스트 삽입
+                  "Oeschinen Lake Campground",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold // 텍스트 강조 설정
+                  ),
+                ),
+              ),
+              // 컬럼의 두번째 자식으로 텍스트 삽입
+              Text(
+                'Kandersteg, Switzerland',
+                style: TextStyle(
+                    color: Colors.grey[500] // 텍스트의 색상을 설정
+                ),
+              )
+            ],
+          ),
+        ),
+        // 두번째 자식 아이콘
+        // Icon(
+        //   Icons.star, // 별모양 아이콘 삽입
+        //   color: Colors.red[500], // 빨간색으로 설정
+        // ),
+        // // 세번째 자식
+        // Text('43')  // 텍스트 표시
+        FavoriteWidget()
+      ],
+    ),
+  );
+
+  // 버튼과 텍스트로 구성된 컬럼을 생성하여 반환하는 함수
+  Column _buildButtonColumn(Color color,IconData icon, String label){
+    // 컬럼을 생성하여 반환
+    return Column(
+      mainAxisSize: MainAxisSize.min, // 여유공간을 최소로 할당
+      mainAxisAlignment: MainAxisAlignment.center, // 가운데 정렬
+      // 컬럼의 자식들로 아이콘과 컨테이너를 등록
+      children: <Widget>[
+        Icon(
+          icon,
+          color: color,
+        ),
+        Container(
+          margin: const EdgeInsets.only(top: 8),  // 컨테이너 상단에 8픽셀의 마진을 할당
+          child: Text(  // 텍스트 할당
+            label,
+            style: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.w400,
+                color: color
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
+  // 텍스트로 구성된 컨테이너를 구현하는 위젯
+  Widget textSection = Container(
+    padding: const EdgeInsets.all(32),
+    child: Text(
+      'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+          'Alps. Situated 1,578 meters above sea level, it is one of the '
+          'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+          'half-hour walk through pastures and pine forest, leads you to the '
+          'lake, which warms to 20 degrees Celsius in the summer. Activities '
+          'enjoyed here include rowing, and riding the summer toboggan run.',
+      softWrap: true, // 텍스트가 영역을 넘어갈 경우 줄바꿈 여부
+    ),
+  );
 }
 
+// StatefulWidget을 상속하는 클래스
+class FavoriteWidget extends StatefulWidget {
+  @override
+  _FavoriteWidgetState createState() => _FavoriteWidgetState();
+}
+class _FavoriteWidgetState extends State<FavoriteWidget>{
+
+  bool _isFavorited = true;
+  int _favoriteCount = 41;
+
+  @override
+  Widget build(BuildContext context) {
+    // 로우를 생성
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        // 컨테이너 생성
+        Container(
+          padding: EdgeInsets.all(0),
+          child: IconButton(
+              icon: (_isFavorited ? Icon(Icons.star) : Icon(Icons.star_border)),
+              color: Colors.red[500],
+              // 아이콘 버튼을 클릭하면 _toggleFavorite 함수를 호출
+              onPressed: _toggleFavorite
+          ),
+        ),
+        // 텍스트를 출력하는 SizedBox 추가
+        SizedBox(
+          width: 18,
+          child: Container(
+            child: Text('$_favoriteCount'),
+          ),
+        )
+      ],
+    );
+  }
+
+  // 아이콘 버튼을 클릭하면 호출
+  void _toggleFavorite() {
+    setState(() {
+      if(_isFavorited){
+        _favoriteCount -= 1;
+        _isFavorited = false;
+      }else{
+        _favoriteCount += 1;
+        _isFavorited = true;
+      }
+    });
+  }
+}
